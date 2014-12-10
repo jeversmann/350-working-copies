b3943329d5d46b4c9623232381ae744a248bbc59
Josh Eversmann <josh.eversmann@utexas.edu>
2014-12-10 16:51:19 -0600

    Combined with a processor

--- screen.v
+++ screen.v
@@ -42,24 +42,422 @@ module screen(
   inout                   I2C_SDA
 );
 
+//=======================================================
+//  PARAMETER declarations
+//=======================================================
+// Not sure if these should be 4'
+parameter I = 3'h0;
+parameter F = 3'h1;
+parameter F1 = 3'h6;
+parameter F2 = 3'h7;
+parameter D = 3'h2;
+parameter X = 3'h3;
+parameter M = 3'h4;
+parameter W = 3'h5;
+// Execute ops
+parameter ADD = 4'h0;
+parameter SUB = 4'h1;
+parameter MUL = 4'h2;
+parameter SET = 4'h3;
+parameter NOP = 4'h4;
+parameter SHFT = 4'h5;
+parameter CMP = 4'h6;
+// Memory ops
+parameter MEM_LD = 4'h0;
+parameter MEM_ST = 4'h1;
+// NOP from above reused
+// Write Back OPS
+parameter WB_REG = 4'h0;
+parameter WB_PC = 4'h1;
+// NOP from above reused
 
 
 //=======================================================
-//  REG/WIRE declarations
+//  PORT declarations
 //=======================================================
 
+//////////// LED //////////
+output               [7:0]              LEDG;
+output               [9:0]              LEDR;
 
-wire[11:0] x;
-wire[11:0] y;
+//////////// KEY //////////
+input                                   CPU_RESET_n;
+input                [3:0]              KEY;
 
-reg clk25 = 0;
-wire resetn;
+//////////// SW //////////
+input                [9:0]              SW;
 
-reg[9:0] count = 0;
-reg[7:0] char = 8'h30;
+//////////// SEG7 //////////
+output               [6:0]              HEX0;
+output               [6:0]              HEX1;
+output               [6:0]              HEX2;
+output               [6:0]              HEX3;
 
+
+/////////////////////////
+// The processor state //
+/////////////////////////
+
+reg [15:0]regs[7:0];     // register
+reg [15:0]pc;             // the pc
+reg [2:0]cur_state = I;
+reg [2:0]next_state = F;
+
+wire clk = KEY[0];        // single step using key0
+initial begin
+        pc = 0;
+        regs[0] = 0;
+        regs[1] = 0;
+        regs[2] = 0;
+        regs[3] = 0;
+        regs[4] = 0;
+        regs[5] = 0;
+        regs[6] = 0;
+        regs[7] = 0;
+end
+
+///////////
+// fetch //
+///////////
+
+reg mem_wren;
+reg next_mem_wren;
+wire [15:0]mem_out;
+ram ((next_state == F) ? pc : next_xv_out, clk, vd, mem_wren, mem_out);
+
+///////////////////
+// decode & regs //
+///////////////////
+
+// Fields in the instruction
+reg [15:0]inst;           // the instruction
+reg [3:0]x_op;
+reg [3:0]m_op;
+reg [3:0]wb_op;
+reg [3:0]next_x_op;
+reg [3:0]next_m_op;
+reg [3:0]next_wb_op;
+wire [4:0] opcode = mem_out[15:11];
+wire [2:0] rd = mem_out[10:8];
+wire [2:0] ra = mem_out[7:5];
+wire [2:0] rb = mem_out[2:0];
+// Immediate values, sign extended using our custom module
+wire [15:0]imm5 = $signed(mem_out[4:0]);
+wire [15:0]imm8 = $signed(mem_out[7:0]);
+// Values loaded from registers
+wire [15:0] va = regs[ra];
+wire [15:0] vb = regs[rb];
+wire [15:0] vd = regs[rd];
+// Values for execute stage
+reg [15:0]next_xv_0;
+reg [15:0]next_xv_1;
+reg [15:0]next_xv_out;
+reg [15:0]xv_0;
+reg [15:0]xv_1;
+reg [15:0]xv_out;
+reg [2:0] next_rd;
+
+/////////////
+// execute //
+/////////////
+
+reg [15:0] nextpc;        // the next pc
+reg [15:0] rfdata;         // the register value
+
+always @(*) begin
+    next_xv_out = 16'hface; // This shouldn't be read
+    case (cur_state)
+        I: begin
+        end
+        F: begin
+        end
+        D: begin
+            case (opcode)
+            // Add, f = 0
+            5'b00000: begin
+                next_x_op = ADD;
+                next_m_op = NOP;
+                next_xv_0 = va;
+                next_xv_1 = imm5;
+                next_wb_op = WB_REG;
+            end
+
+            // Add, f = 1
+            5'b00001: begin
+                next_x_op = ADD;
+                next_m_op = NOP;
+                next_xv_0 = va;
+                next_xv_1 = vb;
+                next_wb_op = WB_REG;
+            end
+
+            // Slt, f = 0
+            5'b00100: begin
+                next_x_op = SET;
+                next_m_op = NOP;
+                next_xv_0 = va;
+                next_xv_1 = imm5;
+                next_wb_op = WB_REG;
+            end
+
+            // Slt, f = 1
+            5'b00101: begin
+                next_x_op = SET;
+                next_m_op = NOP;
+                next_xv_0 = va;
+                next_xv_1 = vb;
+                next_wb_op = WB_REG;
+            end
+
+            // Lea, f = 0
+            5'b11000: begin
+                next_x_op = ADD;
+                next_m_op = NOP;
+                next_xv_0 = va;
+                next_xv_1 = imm5;
+                next_wb_op = WB_REG;
+            end
+
+            // Lea, f = 1
+            5'b11001: begin
+                next_x_op = ADD;
+                next_m_op = NOP;
+                next_xv_0 = pc;
+                next_xv_1 = imm8;
+                next_wb_op = WB_REG;
+            end
+
+            // Call, f = 0
+            5'b11010: begin
+                next_x_op = ADD;
+                next_m_op = NOP;
+                next_xv_0 = va;
+                next_xv_1 = imm5;
+                next_wb_op = WB_PC;
+            end
+
+            // Call, f = 1
+            5'b11011: begin
+                next_x_op = ADD;
+                next_m_op = NOP;
+                next_xv_0 = pc;
+                next_xv_1 = imm8;
+                next_wb_op = WB_PC;
+            end
+
+            // brz, f = 0
+            5'b11110: begin
+                next_x_op = ADD;
+                next_m_op = NOP;
+                next_xv_0 = va;
+                next_xv_1 = imm5;
+                if (vd == 0)
+                    next_wb_op = WB_PC;
+                else
+                    next_wb_op = NOP;
+            end
+
+            // brz, f = 1
+            5'b11111: begin
+                next_x_op = ADD;
+                next_m_op = NOP;
+                next_xv_0 = pc;
+                next_xv_1 = imm8;
+                if (vd == 0)
+                    next_wb_op = WB_PC;
+                else
+                    next_wb_op = NOP;
+            end
+
+            // ld, f = 0
+            5'b10100: begin
+                next_x_op = ADD;
+                next_m_op = MEM_LD;
+                next_xv_0 = va;
+                next_xv_1 = imm5;
+                next_wb_op = WB_REG;
+            end
+
+            // ld, f = 1
+            5'b10101: begin
+                next_x_op = ADD;
+                next_m_op = MEM_LD;
+                next_xv_0 = pc;
+                next_xv_1 = imm8;
+                next_wb_op = WB_REG;
+            end
+
+            // st, f = 0
+            5'b10110: begin
+                next_x_op = ADD;
+                next_m_op = MEM_ST;
+                next_xv_0 = va;
+                next_xv_1 = imm5;
+                next_wb_op = NOP;
+            end
+
+            // st, f = 1
+            5'b10111: begin
+                next_x_op = ADD;
+                next_m_op = MEM_ST;
+                next_xv_0 = pc;
+                next_xv_1 = imm8;
+                next_wb_op = NOP;
+            end
+         endcase
+        end
+        X: begin
+            case (x_op)
+            ADD: begin
+                 next_xv_out = xv_0 + xv_1;
+            end
+            NOP: begin
+            end
+            SET: begin
+                 next_xv_out = xv_0 < xv_1;
+            end
+            endcase
+        end
+        W: begin
+        end
+    endcase
+end
+
+
+///////////////////
+// debug support //
+///////////////////
+reg [15:0]debug;
+assign LEDG[0] = cur_state == I;
+assign LEDG[1] = cur_state == F;
+assign LEDG[2] = cur_state == D;
+assign LEDG[3] = cur_state == X;
+assign LEDG[4] = cur_state == M;
+assign LEDG[5] = cur_state == W;
+assign LEDR = pc[9:0];
+display(debug[15:12], HEX3);
+display(debug[11:8], HEX2);
+display(debug[7:4], HEX1);
+display(debug[3:0], HEX0);
+
+// what do we display
+always @(*) begin
+    if (SW[3]) begin
+        debug = inst;
+    end else if(SW[9]) begin
+        debug = xv_out;
+    end else if(SW[8]) begin
+        debug = next_xv_out;
+    end else if(SW[7]) begin
+        debug = imm5;
+    end else if(SW[6]) begin
+        debug = mem_out;
+    end else if(SW[5]) begin
+        debug[15:12] = rd;
+        debug[11:8] = ra;
+        debug[7:4] = rb;
+    end else begin
+        debug = regs[SW[2:0]];
+    end
+end
+
+/////////////////////////
+// The sequential part //
+/////////////////////////
+
+always @(posedge clk) begin
+    if (!CPU_RESET_n) begin
+        next_state <= F;
+        cur_state <= I;
+        pc = 0;
+        regs[0] = 0;
+        regs[1] = 0;
+        regs[2] = 0;
+        regs[3] = 0;
+        regs[4] = 0;
+        regs[5] = 0;
+        regs[6] = 0;
+        inst <= 0;
+    end else
+    case (cur_state)
+    I: begin
+        cur_state <= F;
+        next_state <= D;
+    end
+    F: begin
+        cur_state <= D;
+        next_state <= X;
+    end
+    D: begin
+        inst <= mem_out;
+        m_op <= next_m_op;
+        x_op <= next_x_op;
+        xv_0 <= next_xv_0;
+        xv_1 <= next_xv_1;
+        wb_op <= next_wb_op;
+        cur_state <= X;
+        next_state <= M;
+        next_rd <= rd;
+    end
+    X: begin
+        case (m_op)
+        MEM_LD: begin
+            mem_wren <= 0;
+        end
+        MEM_ST: begin
+            mem_wren <= 1;
+        end
+        NOP: begin
+            mem_wren <= 0;
+        end
+        endcase
+        xv_out <= next_xv_out;
+        cur_state <= M;
+        next_state <= W;
+    end
+    M: begin
+        mem_wren <= 0;
+        case (m_op)
+        MEM_LD: begin
+                rfdata <= mem_out;
+        end
+        NOP: begin
+           rfdata <= xv_out;
+        end
+        endcase
+        cur_state <= W;
+        next_state <= F;
+        case (wb_op)
+        WB_REG: begin
+            pc <= pc + 1;
+        end
+        WB_PC: begin
+            pc <= rfdata;
+        end
+        NOP: begin
+            pc <= pc + 1;
+        end
+        endcase
+    end
+    W: begin
+        // If the target is R7, don't write out the value
+        case (wb_op)
+        WB_REG: begin
+            if (next_rd != 7) regs[next_rd] <= rfdata;
+            if (m_op == MEM_LD) regs[next_rd] <= mem_out;
+        end
+        WB_PC: begin
+        end
+        NOP: begin
+        end
+        endcase
+        cur_state <= F;
+        next_state <= D;
+    end
+    endcase
+end
 //=======================================================
-//  Structural coding
+//  Display stuff
 //=======================================================
 
 wire[7:0] r;
@@ -86,22 +484,12 @@ hdmi(
 // Divide the 50mhz clock in half
 always @(posedge CLOCK_50_B5B) begin
     clk25 <= !clk25;
-    count <= count + 1;
 end
-
-always @(posedge KEY[3])
-	char = char + 8'b1;
-
-assign LEDR = count;
-assign LEDG = g;
-
-display(x[7:4], HEX3);
-display(x[3:0], HEX2);
-display(y[7:4], HEX1);
-display(y[3:0], HEX0);
-
 endmodule
 
+//////////////////////
+// ASCII TRANSLATOR //
+//////////////////////
 module ascii(clk, mem_addr, mem_value,
   x, y, r, g, b );
 
